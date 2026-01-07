bda4ab9415bafcb82f6971011d1c61d5c489aedc
abudarda12 <abudardaansari66@gmail.com>
2026-01-07 22:49:28 +0530

    subarray

--- Array.java
+++ Array.java
@@ -2,7 +2,7 @@
 public class Array {
 
     public static void main(String[] args) {
-        int arr[] = {2,3,4,5,6,7,8,9 };
+        int arr[] = {2, 4, 6, 8,10};
 
         //int key = 3;
         // for(int i = 0; i < arr.length; i++){
@@ -13,14 +13,12 @@ public class Array {
         //largestNum(arr);
         //System.out.print(binarySearch(arr, key));
         //reverseArray(arr);
-        
         // for(int i = 0; i<arr.length; i++){
         //     System.out.print(" "+arr[i]);
         // }
-
         //pairs(arr);
-
-        subArrays(arr);
+        //subArrays(arr);
+        maxSubArray(arr);
 
     }
 
@@ -56,12 +54,12 @@ public class Array {
         }
         return -1;
     }
-    
+
     //Reverse an array
     public static void reverseArray(int arr[]) {
-        int first = 0, last = arr.length-1;
+        int first = 0, last = arr.length - 1;
 
-        while(first <= last){
+        while (first <= last) {
             int a = arr[first];
             arr[first] = arr[last];
             arr[last] = a;
@@ -72,32 +70,56 @@ public class Array {
     }
 
     //pairs of an array
-    public static void pairs(int arr[]){
+    public static void pairs(int arr[]) {
         int tp = 0;
-        for(int i = 0; i < arr.length; i++){
-            for(int j = i+1; j < arr.length; j++){
-                System.out.print("(" +arr[i]+","+arr[j]+")" );
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + arr[i] + "," + arr[j] + ")");
                 tp++;
             }
             System.out.println();
         }
-        System.out.println("Total no of pair: "+tp);
+        System.out.println("Total no of pair: " + tp);
     }
 
     //print subarrays
-    public static void subArrays(int arr[]){
-        for(int i = 0; i < arr.length; i++){
-            System.out.print("{");
-            for(int j = i; j < arr.length; j++){
-                
-                System.out.print(arr[j] + ",");
-                
+    public static void subArrays(int arr[]) {
+        int ts = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = i; j < arr.length; j++) {
+                int end = j;
+                for (int k = start; k <= end; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                ts++;
+                System.out.println();
             }
-            System.out.print("}");
             System.out.println();
         }
-        
+        System.out.print(ts);
     }
 
-}
+    //maximum Subarrays Sum
+    public static void maxSubArray(int arr[]) {
+        int Max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            int curr = 0;
+            for (int j = i; j < arr.length; j++) {
+                int end = j;
+                
+                for (int k = start; k <= end; k++) {
+                    curr = curr + arr[k];
+                }
+                System.out.println(curr);  
+            }
+            if(Max < curr){
+                Max = curr;
+            }
 
+        }
+        System.out.print(Max);
+    }
+}
